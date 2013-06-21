2a5c1dbc2c7475d32626d73f1d280ae6e2113e00
Jan-Hendrik Willms <tleilax+github@gmail.com>
2013-06-21 23:17:04 +0200

    some initial current data

--- index.php
+++ index.php
@@ -2,4 +2,11 @@
 require 'bootstrap.php';
 
 header('Content-Type: text/plain');
-echo $db->query("SELECT COUNT(*) FROM `weather_data`")->fetchColumn() . ' entries in db';
+
+$last = $db->query("SELECT * FROM `weather_data` ORDER BY `timestamp` DESC LIMIT 1")->fetch(PDO::FETCH_ASSOC);
+echo 'Last update: ' . date('d.m.Y H:i:s', $last['timestamp']) . PHP_EOL;
+echo 'Currently raining: ' . ($last['raining'] ? 'Yes' : 'No') . PHP_EOL;
+var_dump($last);
+
+echo '--' . PHP_EOL;
+echo $db->query("SELECT COUNT(*) FROM `weather_data`")->fetchColumn() . ' entries in db' . PHP_EOL;
