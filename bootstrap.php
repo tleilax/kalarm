2d05382addc20c987e319d7cb79a07f41d57a195
Jan-Hendrik Willms <tleilax+github@gmail.com>
2013-06-21 22:18:09 +0200

    extract setup.php

--- bootstrap.php
+++ bootstrap.php
@@ -12,16 +12,3 @@
 
     $db = new PDO('sqlite:kalarm.sqlite');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $res = $db->exec("CREATE TABLE IF NOT EXISTS `weather_data` (
-        `timestamp` INTEGER PRIMARY KEY,
-        `temperature` DECIMAL,
-        `temperature_felt` DECIMAL,
-        `wind_direction` CHAR(3),
-        `wind_speed` DECIMAL,
-        `humidity` DECIMAL,
-        `precipitation` DECIMAL,
-        `raining` INTEGER,
-        `luminous_intensity` DECIMAL,
-        `radiation` DECIMAL,
-        `air_pressure` DECIMAL
-    )");
---
+++ setup.php
@@ -0,0 +1,16 @@
+<?php
+    require 'bootstrap.php';
+
+    $db->exec("CREATE TABLE IF NOT EXISTS `weather_data` (
+        `timestamp` INTEGER PRIMARY KEY,
+        `temperature` DECIMAL,
+        `temperature_felt` DECIMAL,
+        `wind_direction` CHAR(3),
+        `wind_speed` DECIMAL,
+        `humidity` DECIMAL,
+        `precipitation` DECIMAL,
+        `raining` INTEGER,
+        `luminous_intensity` DECIMAL,
+        `radiation` DECIMAL,
+        `air_pressure` DECIMAL
+    )");
